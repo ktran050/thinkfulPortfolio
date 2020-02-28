a24bec4d07cdf6c1f255ccc89b51c7564b93e7eb
Kevin Tran <ktran050@gmail.com>
2020-02-27 19:05:36 -0800

    added js array to store projects; added a render function

---
+++ projects.js
@@ -0,0 +1,32 @@
+projectList = [
+  {
+    name: "Quiz App",
+    description:
+      "A Disneyland quiz meant to showcase my progress in responsive, and scalable design as well as jQuery",
+    technologies: ["html", "css", "js"],
+    liveLink: "https://ktran050.github.io/quizApp/index.html",
+    repoLink: "https://github.com/ktran050/quizApp",
+    img: "https://i.picsum.photos/id/1008/200/200.jpg"
+  }
+];
+
+function renderProjects() {
+  let projectsHTML = `
+    <div class="pictureSide">QuizApp
+      <img src="https://i.picsum.photos/id/1008/200/200.jpg" alt="">
+      <p>"A Disneyland quiz meant to showcase my progress in responsive, and scalable design as well as jQuery",</p>
+    </div>
+    <div class="linkSide">
+      <p>Live app: <a href="https://ktran050.github.io/quizApp/index.html">github.io</a></p>
+      <p>Repo link: <a href="https://github.com/ktran050/quizApp">Github Repo</a></p>
+      <ul>Technologies used
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        </ul>
+    </div>
+  `;
+  $(".projects").html(projectsHTML);
+}
+
+$(renderProjects());
