6103bed9bb8b600bdf574b401c09a480d32fd1ba
Kevin Tran <ktran050@gmail.com>
2020-03-09 17:55:08 -0700

    Moved layout, fixed links, changed img formating

--- projects.js
+++ projects.js
@@ -12,32 +12,30 @@ projectList = [
 
 function renderProjects() {
   const projectsHTML = `
+  <h1> Projects</h1><br>
   <div class="pictureSide">
-    QuizApp
+    <h3>QuizApp</h3>
     <img
-      src="https://i.picsum.photos/id/1008/200/200.jpg"
+      src="https://user-images.githubusercontent.com/9054819/76269070-3398e380-6268-11ea-8f84-f644ac9f788b.PNG"
       alt=""
       class="projectImg"
-    />
+    /><br>
     <p>
-      "A Disneyland quiz meant to showcase my progress in responsive, and scalable
-      design as well as jQuery",
-    </p>
+    <div class="link-list"><a href="https://ktran050.github.io/quizApp/index.html">Live Demo</a><a href="https://github.com/ktran050/quizApp">Github Repo</a></div>
+  </p>
   </div>
   <div class="linkSide">
-    <p>
-      Live app:
-      <a href="https://ktran050.github.io/quizApp/index.html">github.io</a>
-    </p>
-    <p>
-      Repo link: <a href="https://github.com/ktran050/quizApp">Github Repo</a>
-    </p>
-    <ul>
-      Technologies used
-      <li>html</li>
-      <li>css</li>
-      <li>js</li>
-    </ul>
+  <p>
+  A Disneyland quiz meant to showcase my progress in responsive, and scalable
+  design as well as jQuery
+</p>
+  <div>
+    <h3> Made With: </h3>
+    <div class="badge-list">
+      <div><img src="https://www.pinclipart.com/picdir/big/74-746817_contact-us-for-a-free-consultation-and-quote.png" alt="HTML5 Logo" class="big-badge"></div>
+      <div><img src="https://www.pinclipart.com/picdir/big/175-1759459_eng-a-med-kamel-frameworks-css-css-logo.png" alt="CSS3 Logo" class="big-badge"></div>
+      <div><img src="https://www.pinclipart.com/picdir/big/371-3712206_javascript-javascript-logo-png-clipart.png" alt="JS5 Logo" class="big-badge"></div>
+      </div>
   </div>
   `;
   $(".projects").html(projectsHTML);
